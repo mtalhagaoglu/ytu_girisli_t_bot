29929c86803a519a7c6e7b77dfa18e6edccd5545
mtalhagaoglu <agaoglutalha@gmail.com>
2021-08-07 22:16:20 +0300

    new commands

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def groups(update, context):
         if(result):
             for i in wa_links:
                 if(i[0] == result[7]):
-                    message = "{} için aşağıdaki bölüm gruplarına katılabilirsin!\n\n {}\n\nFakülte grupları için de aşağıdaki bağlantıları kullanabilirsin!\n\n{}\n\nGenel okul gruplarına katılmayı da unutma ;)\n\n{}".format(i[0],i[1].replace(" ","\n"),i[2].replace(" ","\n"),i[3])
+                    message = "{} için aşağıdaki bölüm gruplarına katılabilirsin!\n\n {}\n\nFakülte grupları için de aşağıdaki bağlantıları kullanabilirsin!\n\n{}\n\nGenel okul gruplarına katılmayı da unutma ;)\n\n{}".format(i[0],i[1].replace(" ","\n"),i[2].replace(" ","\n"),f"{i[3]}\nhttps://t.me/ytu2021girisliler")
                     update.message.reply_text(message)
                     return 
             update.message.reply_text("Bölümün için kurulmuş grup bulamadık! Bizimle sosyal medya hesaplarımı üzerinden iletişime geç!")
@@ -38,17 +38,26 @@ def groups(update, context):
             update.message.reply_text("Adına bir başvuru bulamadık. Formu tekrar doldurur musun?\n\nhttps://forms.gle/4C24TSRFMSBRLRoAA")
     except NameError as e:
         update.message.reply_text(f"Bir sorunla karşılaştık :'( en kısa zamanda çözmeye çalışacağız")
+
+def commands(update,context):
+     update.message.reply_text(f"Bot ile kullanabileceğin komutlar şu şekilde:\n /gruplar => Bölümünle ilgili gruplara girmeni sağlar\n /komutlar => Bot ile kullanabileceğin komutları görmeni sağlar\n /linkler => Okul hayatında yardımcı olabilecek linkleri görebilirsin")
     
+def links(update,context):
+    update.message.reply_text(f"Yıldızlıların mobil uygulması, Yıldız Cep! https://yildizcep.com")
 
 def start(update, context):
     first_name = update["message"]["chat"]["first_name"]
-    update.message.reply_text(f"Merhaba {first_name}!\nGruplara katılabilmen için https://forms.gle/4C24TSRFMSBRLRoAA formu doldurmalısın! Sonrasında /gruplar komutu ile katılabileceğin Whatsapp ve Telegram gruplarının bağlantılarını alabilirsin!")
+    update.message.reply_text(f"Merhaba {first_name}!\nGruplara katılabilmen için https://forms.gle/4C24TSRFMSBRLRoAA formu doldurmalısın! Sonrasında /gruplar komutu ile katılabileceğin Whatsapp ve Telegram gruplarının bağlantılarını alabilirsin!\n\n Kullanabileceğin diğer komutlar için /komutlar")
+
 def main():
     updater = Updater(token, use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("gruplar", groups))
+    dp.add_handler(CommandHandler("komutlar", commands))
+    dp.add_handler(CommandHandler("linkler", links))
+
     dp.add_error_handler(error)
 
     updater.start_polling()
